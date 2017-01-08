4edf818b2aaaa9d924b0cd2916b156ddf46eaf65
eoinahern <eoin.ahern@mycit.ie>
2017-01-08 12:44:53 +0000

    using mockito import

---
+++ app/src/test/java/com/example/eoin_a/kotlin_news_app/presentation/Utils/SharedPrefsHelperImpTest.kt
@@ -0,0 +1,19 @@
+package com.example.eoin_a.kotlin_news_app.presentation.Utils
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+/**
+ * Created by eoin_a on 08/01/2017.
+ */
+class SharedPrefsHelperImpTest {
+
+
+
+    @Before
+    fun setUp() {
+
+    }
+
+}
